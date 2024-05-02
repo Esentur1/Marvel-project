c8e7ca376c8cba57c98ff1d0680acd58e0aa1434
Dzhes <esentur1997@gmail.com>
2024-05-02 06:39:47 -0900

    create component RandomChar

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,9 @@
 
 
 class MarvelService {
+    _apiBase = "https://gateway.marvel.com:443/v1/public/";
+    _apiKey = "apikey=9fa8a4e7dd4fa0ac1bd263c9c97fcd0f"
+
     getResource = async (url) => {
         let res = await fetch(url);
 
@@ -11,9 +14,45 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`https://gateway.marvel.com:443/v1/public/characters?apikey=9fa8a4e7dd4fa0ac1bd263c9c97fcd0f`);
+    //getAllCharacters Получает списки комических персонажей с дополнительными фильтрами
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(item => this._transformCharacter(item))
     } 
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    } 
+
+    _transformCharacter = (char) => {
+        if (char.description === "") {
+            return {
+                name: char.name,
+                description: "No character description",
+                thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+                homepage: char.urls[0].url,
+                wiki: char.urls[1].url
+            }
+        } 
+
+        if (char.description.length > 225) {
+            return {
+                name: char.name,
+                description: char.description.slice(0, 225) + "...",
+                thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+                homepage: char.urls[0].url,
+                wiki: char.urls[1].url
+            }
+        }
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
+    }
 }
 
 export default MarvelService;
