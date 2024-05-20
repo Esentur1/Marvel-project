e27c4f3c3e12ad681889071799e4512b5b41ad62
Dzhes <esentur1997@gmail.com>
2024-05-20 09:36:01 -0900

    build project

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,52 +1,164 @@
 import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
+import PropTypes from 'prop-types';
+import React, {Component} from 'react';
+import MarvelService from '../../services/MarvelService'; 
+import Spinner from '../spinner/spinner';
 
-const CharList = () => {
+class CharList extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+        chars: [],
+        loading: true,
+        newItemLoading: false,
+        offset: 210,
+        charEnded: false
+    }
+
+    itemsRef = [];
+
+    setRef = (ref) => {
+        this.itemsRef.push(ref);
+        console.log(this.itemsRef);
+    }
+
+    focusCharSelected = (id) => {
+        this.itemsRef.forEach(item => {
+            item.classList.remove("char__item_selected");
+        })
+        this.itemsRef[id].classList.add("char__item_selected");
+        this.itemsRef[id].focus();
+    }
+
+    marvelService = new MarvelService();
+
+    onCharList = () => {
+        this.onRequest();
+    }
+
+    onCharListLoaded = (newChars) => {
+        let ended = false;
+
+        if (newChars.length < 9) {
+            ended = true;
+        }
+
+        this.setState(({chars, offset}) => ({
+            chars: [...chars, ...newChars],
+            loading: false,
+            newItemLoading: false,
+            offset: offset + 9,
+            charEnded: ended
+        }))
+    }
+
+    onCharListLoading = () => {
+        this.setState({
+            newItemLoading: true
+        })
+    }
+
+    componentDidMount() {
+        this.onCharList();
+    }
+
+    onRequest = (offset) => {
+        this.onCharListLoading();
+        this.marvelService.getAllCharacters(offset)
+            .then(this.onCharListLoaded)
+    }
+
+
+
+    render () {
+        const {chars, loading, newItemLoading, offset, charEnded} = this.state;
+        const {onCharSelected} = this.props;
+    
+        const elements = chars.map((item, i) => {
+            let imgStyle = "";
+            if (item.thumbnail === "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg") {
+                imgStyle = <img style ={{objectFit: "contain"}} src={item.thumbnail} alt="No character description"/>
+            } else {
+                imgStyle = <img style ={{objectFit: "cover"}} src={item.thumbnail} alt="No character description"/>
+            }
+            return (
+                <CharItem 
+                ref={this.setRef}
+                name={item.name} 
+                imgStyle={imgStyle} 
+                key={item.id} 
+                onCharSelected={() => onCharSelected(item.id)}
+                focusCharSelected={() => this.focusCharSelected(i)}
+                />
+            )
+        })
+        
+        const content = loading ? <Spinner/> : <ul className='char__grid'>{elements}</ul>
+
+        return (
+            <div className="char__list">
+                {content}
+                <button 
+                className="button button__main button__long"
+                disabled={newItemLoading}
+                onClick={() => this.onRequest(offset)}
+                style={{'display': charEnded ? 'none' : 'block'}}
+                >
+                    <div className="inner">load more</div>
+                </button>
+            </div>
+        )
+    }
+    
+}
+
+// const CharItem = ({name, imgStyle, onCharSelected}) => {
+//     return (
+//         <li className="char__item" onClick={onCharSelected}>
+//                 {imgStyle}
+//                 <div className="char__name">{name}</div>
+//         </li>
+//     )
+// }
+
+const CharItem = React.forwardRef((props, ref) => {
+    const {name, imgStyle, onCharSelected, focusCharSelected} = props;
     return (
-        <div className="char__list">
-            <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-            </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
+        <li className="char__item" 
+        onClick={() => {
+            onCharSelected()
+            focusCharSelected()
+        }} 
+        ref={ref}
+        >
+            {imgStyle}
+            <div className="char__name">{name}</div>
+        </li>
     )
+})
+
+// class CharItem extends Component {
+//     constructor(props) {
+//         super(props);
+//     }
+
+//     render() {
+//         const {name, imgStyle, onCharSelected} = this.props;
+//         return (
+//             <li className="char__item" onClick={
+//             () => 
+//             {onCharSelected()}}>
+//                 {imgStyle} <div className="char__name">{name}</div>
+//             </li>
+//         )
+//     }
+
+// }
+
+CharList.propTypes = {
+    onCharSelected: PropTypes.func
 }
 
 export default CharList;
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,7 +2,8 @@
 
 class MarvelService {
     _apiBase = "https://gateway.marvel.com:443/v1/public/";
-    _apiKey = "apikey=9fa8a4e7dd4fa0ac1bd263c9c97fcd0f"
+    _apiKey = "apikey=9fa8a4e7dd4fa0ac1bd263c9c97fcd0f";
+    _baseOffset = 210;
 
     getResource = async (url) => {
         let res = await fetch(url);
@@ -15,8 +16,8 @@ class MarvelService {
     }
 
     //getAllCharacters Получает списки комических персонажей с дополнительными фильтрами
-    getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(item => this._transformCharacter(item))
     } 
 
@@ -28,29 +29,35 @@ class MarvelService {
     _transformCharacter = (char) => {
         if (char.description === "") {
             return {
+                id: char.id,
                 name: char.name,
                 description: "No character description",
                 thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
                 homepage: char.urls[0].url,
-                wiki: char.urls[1].url
+                wiki: char.urls[1].url,
+                comics: char.comics.items
             }
         } 
 
         if (char.description.length > 225) {
             return {
+                id: char.id,
                 name: char.name,
                 description: char.description.slice(0, 225) + "...",
                 thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
                 homepage: char.urls[0].url,
-                wiki: char.urls[1].url
+                wiki: char.urls[1].url,
+                comics: char.comics.items
             }
         }
         return {
+            id: char.id,
             name: char.name,
             description: char.description,
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items
         }
     }
 }
